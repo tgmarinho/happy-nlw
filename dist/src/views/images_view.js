789adab4b910ab109290e676b88b9326c1953f12
Thiago Marinho <tgmarinho@gmail.com>
2020-10-15 15:31:45 -0400

    heroku

--- dist/src/views/images_view.js
+++ dist/src/views/images_view.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    render(image) {
-        return {
-            url: `${process.env.API_URL}/uploads/${image.path}`
-        };
-    },
-    renderMany(images) {
-        return images.map(image => this.render(image));
-    }
+  render(image) {
+    return {
+      url: `${process.env.API_URL}/${image.path}`,
+    };
+  },
+  renderMany(images) {
+    return images.map((image) => this.render(image));
+  },
 };
