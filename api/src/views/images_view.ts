0a44e537aed62ae9688b294090ea7d8e4aa222fe
Thiago Marinho <tgmarinho@gmail.com>
2020-10-14 20:35:59 -0400

    variavel para API_URL

--- api/src/views/images_view.ts
+++ api/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
   render(image: Image) {
     return {
       
-      url: `http://localhost:3333/uploads/${image.path}`
+      url: `${process.env.API_URL}/uploads/${image.path}`
     }
   },
 
